2a3d6a72899fba67fcd32cb7f5c980c8ab07ca50
Marcos Stefani Rosa <OUR_NAME@example.com>
2021-01-22 23:06:28 -0300

    add wiremock container

---
+++ wiremock/start.sh
@@ -0,0 +1,5 @@
+host="0.0.0.0"
+port="8081"
+
+java -jar wiremock-standalone-2.14.0.jar --port $port #--verbose &> wiremock.log 2>&1 &
+echo "Wiremock started on host $host and port $port. PID : $!"
