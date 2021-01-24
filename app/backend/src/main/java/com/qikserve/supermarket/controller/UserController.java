252e9f587c44b32fc23c894957840ea23a00add8
Marcos Stefani Rosa <OUR_NAME@example.com>
2021-01-24 10:25:08 -0300

    verify if user exists controller

--- app/backend/src/main/java/com/qikserve/supermarket/controller/UserController.java
+++ app/backend/src/main/java/com/qikserve/supermarket/controller/UserController.java
@@ -3,6 +3,7 @@ package com.qikserve.supermarket.controller;
 import com.qikserve.supermarket.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,12 @@ public class UserController {
 
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<?> verify(@RequestParam String user) {
+        if (service.exists(user)) return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/create")
